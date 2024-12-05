ce504f48ca81302529e3acb39936f4795cf2bac6
SujithS10 <sujithraj0307@gmail.com>
2024-12-05 23:09:41 +0530

    single subcls

---
+++ inhertaince.py
@@ -0,0 +1,10 @@
+class dad:
+    def action(self):
+        print("He use left hand")
+
+class first_son(dad):
+    def music(self):
+        print("singing")
+
+first_son = first_son()
+first_son.action()
